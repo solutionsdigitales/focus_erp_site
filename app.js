77d018ca6922f0020f42f01340b808baf880454d
= <jeremielodi@gmail.com>
2021-12-13 23:49:57 +0100

    typo, comptabilite

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 const express = require('express');
 
 const app = express();
-var device = require('express-device');
+const device = require('express-device');
 app.use(device.capture());
 
-// permet au server de recuper le contenu des formulaire (json ou urlencoded)
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json({ limit : '18mb' }));
 app.use(bodyParser.urlencoded({
   extended: true,
   limit : '18mb'
 }));
 
-
-// dossier(s) racine(s) de l'appication
-// pour que le navigateur puisse avoir acces au dossiers client
-//ce qui permet de passer ceci sur l'url =>   http://localhost:8080/index.html
 app.use(express.static(__dirname + '/client'));
 
 // demarrage du serveur
